7a95c15432e929943db11c77bfb5c8797eb2a59d
Quinn Le <quynhnle135@gmail.com>
2023-10-28 16:08:28 -0400

    Updated urls and make login as the root URL

---
+++ README.md
@@ -0,0 +1,24 @@
+# Django ToDoList Application with CRUD operation
+
+With this ToDoList application, you can manage your tasks effectively with basic yet powerful functionalities. 
+
+## Features:
+- User Registration: New users can easily register for an account to personalize their to-do experience.
+- Authentication System: Users can securely log in and log out, ensuring that their tasks are private and protected.
+- Task Management: Users can add, update, search and delete their personal tasks.
+
+## Technology Stack:
+- Back-End: Django, Python.
+- Front-End: HTML, CSS.
+- Version Control: Git, Github.
+
+## Installation:
+
+Clone the repository
+
+```commandline
+git clone https://github.com/quynhnle135/django-todolist-project.git
+```
+
+
+
--- base/urls.py
+++ base/urls.py
@@ -1,14 +1,17 @@
 from django.urls import path
+from django.views.generic import RedirectView
+
 from . import views
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='login/'), name='home'),  # Redirect from the root URL
     path("login/", views.UserLogInView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(next_page="login"), name="logout"),
     path("register/", views.UserRegisterView.as_view(), name="register"),
-    path("", views.TaskList.as_view(), name="task-list"),
-    path("<int:pk>/", views.TaskDetail.as_view(), name="task-detail"),
-    path("create/", views.TaskCreate.as_view(), name="task-create"),
-    path("<int:pk>/update", views.TaskUpdate.as_view(), name="task-update"),
-    path("<int:pk>/delete", views.TaskDelete.as_view(), name="task-delete"),
+    path("tasks/", views.TaskList.as_view(), name="task-list"),
+    path("tasks/<int:pk>/", views.TaskDetail.as_view(), name="task-detail"),
+    path("tasks/create/", views.TaskCreate.as_view(), name="task-create"),
+    path("tasks/<int:pk>/update", views.TaskUpdate.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete", views.TaskDelete.as_view(), name="task-delete"),
 ]
